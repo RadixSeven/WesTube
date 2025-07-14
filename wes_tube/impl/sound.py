e77a5413f10549f62ab2778768bf2f9a07513ffd
RadixSeven <compu.moyer@gmail.com>
2025-07-13 22:49:03 -0400

    Write pseudocode for detect_offset
    This is complete for the first pass but
    needs more work for the second pass.

--- wes_tube/impl/sound.py
+++ wes_tube/impl/sound.py
@@ -36,6 +36,47 @@ def detect_offset(filename: Path) -> int:
         RuntimeError: If the video file can't be processed.
     """
     # TODO: Implement sound offset detection using PyTorch
+
+    # # Calculate the face trajectories in the first pass through the video file
+    #
+    # Trajectory = list[FaceDetect]
+    # shots = scene_detect(input_video_file)
+    # face_trajectories: dict[ShotBoundaries, list[Trajectory]] = defaultdict(list)
+    # for each frame in input_video:
+    #   frame_shots = [shot for shot in shots if is_in_shot(frame, shot)]
+    #   face_detects = detect_faces(frame)
+    #   for each shot in frame_shots:
+    #      open_trajectories = [t for t in face_trajectories[shot] if
+    #          last frame in t is at most num_failed_det frames ago]
+    #      assign each bounding box to the first open trajectory without an assignment
+    #            yet this frame whose last assigned frame overlaps (intersection/union) by
+    #            more than iou_threshold
+    #   ending_shots = [shot for shot in shots if is_last_frame(frame, shot)]
+    #   # Eliminate too short trajectories and interpolate the rest
+    #   # Eliminate any trajectories that are too small
+    #   for each shot in ending_shots:
+    #       f = [interpolate_missing_bounding_boxes(t) for t in face_trajectories[shot] if
+    #           length(t) > too_small_trajectory_len (a.k.a. opt.min_track)]
+    #       face_trajectories[shot] = [t for t in f if mean bounding box width
+    #            or mean bounding box height is larger than min_face_size]
+    #
+    # # Second pass through the video file
+    # # Run the SyncNet model on frames cropped to each face trajectory
+    # # Maintain a window of batch_size frames and the associated audio.
+    # # For each trajectory, crop each frame in the window to the trajectory.
+    # # then run the SyncNet model on those frames and record the offset calculated
+    # # from those frames.
+    # # Offset is calculated by creating an audio feature vector and a video
+    # # feature vector and calculating the frame offset (-v_shift..+v_shift) at
+    # # which the distance between those vectors is minimized. I do not yet
+    # # understand how the batch size relates to the feature vector size. I'm
+    # # also confused why when I ran the original run_syncnet.py, it created
+    # # 3 face-cropped video files, but only printed one offset. My reading
+    # # of the code makes me expect it to print one offset per face-cropped file.
+    #
+    # # To make the coding more like the research code, I might implement this
+    # # as N passes: one per face in the shot with the maximum number of faces.
+    # TODO: Write pseudocode for the second and later passes
     with resources.open_binary(assets, "syncnet_v2.model") as f:
         syncnet_model = f.read()
         logger.info(f"SyncNet Model is {len(syncnet_model)} bytes long")
